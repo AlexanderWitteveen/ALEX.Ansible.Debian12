c52af62c2274ee42ca4e1be783d0b2cadd522d0e
Alexander Witteveen <alex.witteveen@live.nl>
2024-07-16 14:08:23 +0000

    Build 0.1.4

--- roles/configurebase/scripts/remote.install.python3.sh
+++ roles/configurebase/scripts/remote.install.python3.sh
@@ -2,8 +2,9 @@
 
 ipaddress=$1
 username=$2
+basename=$3
 
-expect -f $(dirname "$0")/remote.install.python3.expect "$ipaddress" "$username"
+expect -f $(dirname "$0")/remote.install.python3.expect "$ipaddress" "$username" "$basename"
 if [[ "$?" != "0" ]]; then 
     echo "**** Error: Expect failed"
 fi
